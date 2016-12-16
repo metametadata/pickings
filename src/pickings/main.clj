1fdf77776c255d9aa8191e7a042286e5ce7f4ddb
metametadata <yuri.govor@gmail.com>
2016-12-16 02:00:45 +0200

    fix main file - I forgot to include keylistener key methods

--- src/pickings/main.clj
+++ src/pickings/main.clj
@@ -1,7 +1,13 @@
 (ns pickings.main
   (:gen-class)
   (:require [pickings.core :as core]
+            [pickings.keylistener]
             [integrant.core :as ig]))
+
+; remove default methods to detect undefined lifecycle methods
+(remove-method ig/init-key :default)
+(remove-method ig/halt-key! :default)
+; TODO: add other methods and/or see https://github.com/weavejester/integrant/issues/6
 
 (defn -main
   [& args]
